0a58a01347d88f0dfee61ded2b904c540b5d88f4
ArchLance <lancee_zhang@163.com>
2023-11-09 18:52:48 +0800

    [NSSA_ViolationMonitor]: test datebase success
    Signed-off-by: ArchLance <lancee_zhang@163.com>

--- test_consumer.py
+++ test_consumer.py
@@ -1,5 +1,7 @@
+import yaml
 from kafka import KafkaConsumer
 import pickle
+import pymysql
 
 RESPONSE_PACKET = 1
 REQUEST_PACKET = 0
@@ -25,37 +27,61 @@ def judge_request_or_response(pkt):
         return REQUEST_PACKET
 
 
-def handle_http(pkt):
+def handle_http_for_login(pkt):
     if judge_request_or_response(pkt) == REQUEST_PACKET:
+        if pkt.http.request_uri != '/user/login':
+            return
         request_src = pkt.ip.src
         request_dst = pkt.ip.dst
-        request_method = pkt.http.request_method
-        request_full_uri = pkt.http.request_full_uri
         request_usr_name = pkt['json'].get_field('value_string').all_fields[0].showname_value
-
-        print('%s %s ----> %s' % (request_method, request_src, request_dst))
-        print('%s %s' % (request_full_uri, request_usr_name))
-        print('---------------------')
+        user_dict[(request_usr_name, request_src, request_dst)] = request_src
     else:
+        if pkt.http.response_for_uri[-11:] != '/user/login':
+            return
         response_src = pkt.ip.src
         response_dst = pkt.ip.dst
-        response_response_code = pkt.http.response_code
         response_response_code_desc = pkt.http.response_code_desc
         response_usr_name = pkt['json'].get_field('value_string').all_fields[2].showname_value
-        print('%s %s %s ----> %s' % (response_response_code, response_response_code_desc, response_src, response_dst))
-        print(response_usr_name)
+        if response_response_code_desc == 'OK' \
+                and user_dict.get((response_usr_name, response_dst, response_src)) is not None:
+            cursor = db_connector.cursor()
+            sql = "insert into user_ip_dynamic_table (user_name,user_ip) values (\'%s\',\'%s\')"\
+                  % (response_usr_name, response_dst);
+            print(sql)
+            try:
+                # 执行sql语句
+                cursor.execute(sql)
+                # 提交到数据库执行
+                db_connector.commit()
+            except:
+                # 如果发生错误则回滚
+                db_connector.rollback()
+
+
+def init_config(config_file):
+    with open(config_file, 'r') as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+        return config
 
 
 def main():
-    consumer = KafkaConsumer('net-traffic', bootstrap_servers=['localhost:9092'])
+    consumer = KafkaConsumer(args_config['mq']['traffic_topic'], bootstrap_servers=args_config['mq']['server'])
     for bytes_stream in consumer:
         pkt = pickle.loads(bytes_stream.value)
         if judge_http(pkt):
-            handle_http(pkt)
+            handle_http_for_login(pkt)
 
 
 if __name__ == '__main__':
+    args_config = init_config('./config.yaml')
+    db_connector = pymysql.connect(host=args_config['mysql']['host'],
+                                   port=int(args_config['mysql']['port']),
+                                   user=args_config['mysql']['user'],
+                                   password=args_config['mysql']['passwd'],
+                                   database=args_config['mysql']['db_name'])
+    user_dict = {}
     main()
+    db_connector.close()
 
 # print(pkt['http'])
 # print(type(pkt['json']))
